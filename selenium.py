d6b05ffea68b6d0614122f2065827a44793810b2
fabiodemo <faberdemo@gmail.com>
2024-04-24 22:10:22 -0300

    Save connection and download url access

--- selenium.py
+++ selenium.py
@@ -5,4 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver_path = 'msedgedriver.exe'
 
-# driver = webdriver
+driver = webdriver.Edge(executable_path=driver_path)
+
+driver.get('https://dados.gov.br/dados/conjuntos-dados/serie-historica-de-precos-de-combustiveis-e-de-glp')
+
+
+wait = WebDriverWait(driver, 10)
+button = wait.until(EC.element_to_be_clickable((By.ID, 'btnDownloadUrl')))
+
+button.click()
+
+driver.quit()
