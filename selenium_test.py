e2cf649e4f8b03c28352d6f16a6f2af791f5ca52
fabiodemo <faberdemo@gmail.com>
2024-04-24 22:38:10 -0300

    Changed to download by link clicks

--- selenium_test.py
+++ selenium_test.py
@@ -1,24 +1,37 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.service import Service
+import time
+
 
 driver_path = 'msedgedriver.exe'
+download_path = 'downloads/'
 
-driver = webdriver.Edge(executable_path=driver_path)
+options = webdriver.EdgeOptions()
+options.add_experimental_option('prefs', {
+  "download.default_directory": download_path,
+  "download.prompt_for_download": False,
+  "download.directory_upgrade": True,
+  "safebrowsing.enabled": True
+})
 
+s = Service(driver_path)
+driver = webdriver.Edge(service=s, options=options)
 driver.get('https://dados.gov.br/dados/conjuntos-dados/serie-historica-de-precos-de-combustiveis-e-de-glp')
 
 
-wait = WebDriverWait(driver, 10)
-button = wait.until(EC.element_to_be_clickable((By.ID, 'btnDownloadUrl')))
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.XPATH, "//button[contains(text(), 'Acessar o recurso')]"))
+)
 
-button.click()
+buttons = driver.find_elements(By.XPATH, "//button[contains(text(), 'Acessar o recurso')]")
+print(buttons)
 
-time.sleep(5)
-
-download_link = driver.find_element(By.CLASS_NAME, 'download-link')
-
-file_url = download_link.get_attribute('href')
+for button in buttons:
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(2)
 
 driver.quit()
